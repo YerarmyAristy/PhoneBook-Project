a8b757cbdc0d2abd1224dc669692ffe01bb4eaad
Yerarmy Aristy <107381434+YerarmyAristy@users.noreply.github.com>
2023-05-25 18:22:14 -0400

    Delete phoneBook directory

--- phoneBook/src/main/java/com/SBS/phoneBook/sevice/exception/ContactsNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.SBS.phoneBook.sevice.exception;
-
-public class  ContactsNotFoundException extends RuntimeException {
-    public ContactsNotFoundException(String message) {
-        super(message);
-    }
-}
